b15edb3028fbf34fb5bfc00b9f10aa0ef6507825
Nils Melchert <nils.melchert@imr.uni-hannover.de>
2019-11-11 14:52:02 +0100

    Controllino control working

---
+++ scripts/controllino_ss.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import rospy
+from rosserial_arduino.srv import Test, TestResponse
+
+def callback(req):
+    print(req.input)
+    return TestResponse("")
+
+if __name__ == '__main__':
+    rospy.init_node("controllino_ss_nd")
+    s = rospy.Service('controllino_getter', Test, callback)
+    rospy.spin()
